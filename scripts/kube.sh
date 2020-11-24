a77f9f684e8f056c55c2be4b48f7fc495c1c0b77
Adam Tworkiewicz <adam.tworkiewicz@reddigit7.com>
2020-11-24 17:10:18 -0600

    initial commit

---
+++ scripts/kube.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+wget https://raw.githubusercontent.com/jonmosco/kube-ps1/master/kube-ps1.sh
+
+cat <<EOT >> ~/.profile
+source <(kubectl completion bash)
+alias k=kubectl
+alias kn='kubectl config set-context --current --namespace '
+alias kdr='k --dry-run=client -o=yaml '
+alias kbb='kubectl run busybox-test --image=busybox -it --rm --restart=Never --'
+complete -F __start_kubectl k
+complete -F __start_kubectl kdr
+complete -F __start_kubectl kn
+export KUBE_EDITOR=mcedit
+export ETCDCTL_API=3
+source ~/kube-ps1.sh; PS1='[\u@\h \W $(kube_ps1)]\$ '
+EOT
+
+. ~/.profile
