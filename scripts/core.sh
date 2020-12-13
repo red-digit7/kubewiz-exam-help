a3d4428d90ff3645662fa718e5c9a29e50a417b5
Adam Tworkiewicz <adam.tworkiewicz@reddigit7.com>
2020-12-13 07:38:56 -0600

    added kdf alias

--- scripts/core.sh
+++ scripts/core.sh
@@ -5,12 +5,14 @@ wget -q https://raw.githubusercontent.com/jonmosco/kube-ps1/master/kube-ps1.sh -
 cat <<EOBASHRC >> ~/.bashrc
 source <(kubectl completion bash)
 alias k=kubectl
-alias kn='kubectl config set-context --current --namespace '
-alias kdr='k --dry-run=client -o=yaml '
+alias kn='kubectl config set-context --current --namespace'
+alias kdr='k --dry-run=client -o=yaml'
+alias kdf='k delete --grace-period=0 --force'
 alias kbb='kubectl run busybox-test --image=busybox -it --rm --restart=Never --'
 complete -F __start_kubectl k
 complete -F __start_kubectl kdr
 complete -F __start_kubectl kn
+complete -F __start_kubectl kdf
 export KUBE_EDITOR=vim
 export ETCDCTL_API=3
 source ~/kube-ps1.sh
